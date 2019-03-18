855ac238fa48d60b751a50cf050f95ef84973834
KBhandari11 <32970056+KBhandari11@users.noreply.github.com>
2019-03-18 00:01:06 -0400

    Minor Changes for own website

--- src/baseStyles.js
+++ src/baseStyles.js
@@ -3,11 +3,11 @@
 const styles = {
   overlay(isOpen) {
     return {
-      position: 'fixed',
+      position: 'relative',
       zIndex: 1000,
-      width: '100%',
-      height: '100%',
-      background: 'rgba(0, 0, 0, 0.3)',
+      width: 'inherit',
+      height: 'inherit',
+      background: '#545454',
       opacity: isOpen ? 1 : 0,
       MozTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
       MsTransform: isOpen ? '' : 'translate3d(100%, 0, 0)',
@@ -17,62 +17,44 @@ const styles = {
       transition: isOpen ? 'opacity 0.3s' : 'opacity 0.3s, transform 0s 0.3s'
     };
   },
+//this changes the width and the height of the menu
+  menuWrap: function menuWrap(isOpen, width, right) {
 
-  menuWrap(isOpen, width, right) {
     return {
-      position: 'fixed',
+      background: '$545454',
+      position: 'absolute',
       right: right ? 0 : 'inherit',
       zIndex: 1100,
-      width,
-      height: '100%',
-      MozTransform: isOpen
-        ? ''
-        : right
-        ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
-      MsTransform: isOpen
-        ? ''
-        : right
-        ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
-      OTransform: isOpen
-        ? ''
-        : right
-        ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
-      WebkitTransform: isOpen
-        ? ''
-        : right
-        ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
-      transform: isOpen
-        ? ''
-        : right
-        ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
-      transition: 'all 0.5s'
+      width: '17%',
+      height: '600px',
+      MozTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      MsTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      OTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      WebkitTransform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      transform: isOpen ? '' : right ? 'translate3d(100%, 0, 0)' : 'translate3d(-100%, 0, 0)',
+      transition: 'all 0.0s'
     };
   },
 
-  menu() {
+  menu: function menu() {
     return {
-      height: '100%',
+      height: '75%',
       boxSizing: 'border-box',
-      overflow: 'auto'
+      overflow: 'auto',
+
     };
   },
 
-  itemList() {
+  itemList: function itemList() {
     return {
       height: '100%'
     };
   },
 
-  item() {
+  item: function item() {
     return {
       display: 'block'
     };
   }
 };
-
 export default styles;
